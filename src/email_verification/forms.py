17696ff35fd626f7a89103d5b775f28ebac42745
KASUN677 <70045146+KASUN677@users.noreply.github.com>
2020-10-18 16:10:34 -0400

    added hc fields

--- src/email_verification/forms.py
+++ src/email_verification/forms.py
@@ -13,4 +13,4 @@ class BaseForm(forms.ModelForm):
 class EmailForm(BaseForm):
     class Meta:
         model = Verification
-        fields = ["email"]
+        fields = ["email" , "hcn" , "vc" , "dob" , "postal code", "DNI"]
